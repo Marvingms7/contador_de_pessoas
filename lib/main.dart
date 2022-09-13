09156281c6eb627a13b32b11b719b0f75b332aee
Marcus Vinicius 07 <marcus.vinicius.16041997@gmail.com>
2022-09-13 16:55:01 -0300

    alterando imagens do app

--- lib/main.dart
+++ lib/main.dart
@@ -4,49 +4,59 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: 'Contador de Pessoas',
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        // ignore: prefer_const_literals_to_create_immutables
+      home: Stack(
         children: [
-          const Text(
-            "Pessoas: 500",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          Image.asset(
+            'images/restaurante.jpg',
+            fit: BoxFit.cover,
+            height: 1000.0,
           ),
-          Row(
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            // ignore: prefer_const_literals_to_create_immutables
             children: [
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextButton(
-                    onPressed: () {},
-                    // ignore: prefer_const_constructors
-                    child: Text(
-                      '-1',
-                      style:
-                          const TextStyle(fontSize: 40.0, color: Colors.purple),
-                    )),
+              const Text(
+                "Pessoas: 500",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
               ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextButton(
-                    onPressed: () {},
-                    // ignore: prefer_const_constructors
-                    child: Text(
-                      '+1',
-                      style:
-                          const TextStyle(fontSize: 40.0, color: Colors.purple),
-                    )),
-              )
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextButton(
+                        onPressed: () {},
+                        // ignore: prefer_const_constructors
+                        child: Text(
+                          '-1',
+                          style: const TextStyle(
+                              fontSize: 40.0, color: Colors.purple),
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextButton(
+                        onPressed: () {},
+                        // ignore: prefer_const_constructors
+                        child: Text(
+                          '+1',
+                          style: const TextStyle(
+                              fontSize: 40.0, color: Colors.purple),
+                        )),
+                  )
+                ],
+              ),
+              const Text(
+                "Pode Entrar!",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0,
+                ),
+              ),
             ],
-          ),
-          const Text(
-            "Pode Entrar!",
-            style: TextStyle(
-              color: Colors.white,
-              fontStyle: FontStyle.italic,
-              fontSize: 30.0,
-            ),
-          ),
+          )
         ],
       )));
 }
